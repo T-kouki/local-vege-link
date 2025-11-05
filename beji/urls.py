0494c7f4e85110642b1e3a310e7e9a0e8407d73c
kominami03 <mcd2476007@stu.o-hara.ac.jp>
2025-11-05 14:06:01 +0900

    2回目

--- beji/urls.py
+++ beji/urls.py
@@ -20,6 +20,7 @@ from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import redirect
+from polls import views
 
 def redirect_to_login(request):
     return redirect("/accounts/login/")
@@ -28,4 +29,5 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('menu/', views.menu, name='menu'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+
+def menu(request):
+     return render(request, 'registration/menu.html')
+def cart(request):
+    return render(request,'registration/cart.html') 
