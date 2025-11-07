a3a5cfe3fa21c96ac4c9735eaf0fd79a287f11c9
T-kouki <mcd2476071@stu.o-hara.ac.jp>
2025-11-07 09:42:15 +0900

    11/7 signup version2

--- polls/forms.py
+++ polls/forms.py
@@ -23,8 +23,13 @@ class FarmSignupForm(UserCreationForm):
     farm_name = forms.CharField(max_length=100, label="農園名")
 
     class Meta:
-        model = User
-        fields = ('username', 'farm_name', 'email', 'password1', 'password2')
+        familyname = forms.CharField(max_length=50, label="姓")
+        lastname = forms.CharField(max_length=50, label="名")
+        username = forms.CharField(max_length=150, label="表示される名前")
+        address = forms.CharField(max_length=255, label="住所")
+        phone_number = forms.CharField(max_length=20, label="電話番号")
+        email = forms.EmailField(required=True, label="メールアドレス")
+        image = forms.ImageField(required=False, label="販売実績が確認できる書類")
     class ImageUploadForm(forms.Form):
         username = forms.CharField(max_length=150)
         password = forms.CharField(widget=forms.PasswordInput)
