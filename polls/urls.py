6e5582b9109f8ffa1692c9860eda570cd8a58b11
T-kouki <mcd2476071@stu.o-hara.ac.jp>
2025-11-10 12:01:19 +0900

    11/10 12:00 購入者ログイン接続

--- polls/urls.py
+++ polls/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path("signup/eat/", views.signup_eat, name="signup_eat"),
     path("signup/farm/", views.signup_farm, name="signup_farm"),
     path("farm_menu/", views.farm_menu_view, name="farm_menu"),
-    #path("eat_menu/", views.eat_menu_view, name="eat_menu"),
+    path("eat_menu/", views.eat_menu_view, name="eat_menu"),
     path("login/", views.login_view, name="login"),
     path("product_upload/", views.farm_product_upload, name="product_upload"),
     path("edit_profile/", views.profile_edit, name="edit_profile"),
-   
+
 ]
